e8669e718b7f9ad056cca104ab41da7a90e659b9
RyannKim327 <rksesgundo123@gmail.com>
2025-09-25 19:34:05 +0800

    09-25-25 19:34

--- src/alias.js
+++ src/alias.js
@@ -3,9 +3,10 @@ const { get, post } = require("../utils/gist");
 module.exports = async (api, event, regex) => {
 	const name = event.message.text.match(regex)[1];
 	const users = await get("users.json");
-	if (users[name]) {
+	if (users[event.sender.id]) {
 		return api.sendMessage("This account is already registered", event);
 	}
+	users[event.sender.id] = name;
 	api.sendMessage(`The alias ${name} is not set to this account.`, event);
 	await post("users.json", users);
 };
